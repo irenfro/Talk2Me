d673983dbf8691369286b5f1a46add03721a3b82
DragonSlayer97 <ian@renfro.org>
2015-10-17 00:12:22 -0400

    Initial Commit

---
+++ Talk2Me/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Talk2Me
+//
+//  Created by Ian Renfro on 10/17/15.
+//  Copyright © 2015 SoloMin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
